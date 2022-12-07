21db8dfc92f347c7d01c87239f5b1b2a62137883
dotmantosh <dotuntosh@gmail.com>
2022-12-07 13:19:05 +0100

    add production api url

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const url = "http://localhost:5000";
+const url = "https://data-management-api.vercel.app";
+/* const url = "http://localhost:5000"; */
 const Api = () => {
   return axios.create({
     baseURL: url,
